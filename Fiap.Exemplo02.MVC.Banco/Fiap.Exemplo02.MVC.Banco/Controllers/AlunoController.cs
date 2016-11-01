9ed68d4466e273e01d8e4a40a0aa15ec3e2cf2a9
Felipe Moita Vieira <fel.vieira@gmail.com>
2016-11-01 18:30:12 -0200

    Criacao projeto WEB MVC com banco de Dados, CRUD Parcial

---
+++ Fiap.Exemplo02.MVC.Banco/Fiap.Exemplo02.MVC.Banco/Controllers/AlunoController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Fiap.Exemplo02.MVC.Banco.Models;
+
+namespace Fiap.Exemplo02.MVC.Banco.Controllers
+{
+    public class AlunoController : Controller
+    {
+        private PortalContent _context = new PortalContent();
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Aluno aluno)
+        {
+            
+            
+            _context.Aluno.Add(aluno);
+            _context.SaveChanges();
+            TempData["msg"] = "Aluno Cadastrado";
+            return RedirectToAction("Cadastrar");
+        }
+
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            List<Aluno> _lista = _context.Aluno.ToList();
+            return View(_lista);
+        }
+        
+    }
+}
