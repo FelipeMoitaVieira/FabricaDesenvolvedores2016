ad7b29d9b25ef5a8946d4fed364a0dff02c1c4a3
Felipe Moita Vieira <fel.vieira@gmail.com>
2016-11-07 12:14:06 -0200

    Criação projeto Exercicio 3

---
+++ Exercicio03/Exercicio03/Controllers/ProdutoController.cs
@@ -0,0 +1,38 @@
+﻿using Exercicio03.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exercicio03.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private static List<Produto> _lista = new List<Produto>();
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Produto produto)
+        {
+
+            _lista.Add(produto);
+
+            TempData["msg"] = "Produto Cadastrado com Sucesso";
+
+            return RedirectToAction("Cadastrar");
+        }
+
+
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            return View(_lista);
+        }
+
+    }
+}
---
+++ Exercicio03/Exercicio03/Models/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Exercicio03.Models
+{
+    public class Produto
+    {
+
+        public int ProdutoId { get; set; }
+        [Display(Name ="Descrição")]
+        public string Descricao { get; set; }
+        [Display(Name ="Título")]
+        public string Titulo { get; set; }
+        [Display(Name="Data de Cadastro")]
+        public DateTime DataCadastro { get; set; }
+        public decimal Valor { get; set; }
+        public bool Nacional { get; set; }
+
+    }
+}
