559bb537287c71775e59d42b299b70bbfd7dc362
Felipe Moita Vieira <fel.vieira@gmail.com>
2016-11-24 17:12:54 -0200

    Adição dos arquivos faltantes no último commit - Implementação funcionalidade de autopreenchimento endereço

---
+++ Fiap.Exemplo02.MVC.Banco/Fiap.Exemplo02.Database/Endereco.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Endereco]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Bairro] VARCHAR(50) NOT NULL, 
+    [Cidade] VARCHAR(100) NOT NULL, 
+    [Cep] VARCHAR(8) NOT NULL, 
+    [Logradouro] VARCHAR(150) NOT NULL, 
+    [NomeEstado] VARCHAR(100) NOT NULL, 
+    [Estado] VARCHAR(2) NOT NULL
+)
---
+++ Fiap.Exemplo02.MVC.Banco/Fiap.Exemplo02.MVC.Banco/Controllers/EnderecoController.cs
@@ -0,0 +1,31 @@
+﻿using Fiap.Exemplo02.MVC.Banco.Models;
+using Fiap.Exemplo02.MVC.Banco.UnitsOfWork;
+using System.Web.Mvc;
+
+
+namespace Fiap.Exemplo02.MVC.Banco.Controllers
+{
+    public class EnderecoController : Controller
+    {
+        private UnitOfWork _unit = new UnitOfWork();
+
+
+       [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Cadastrar(Endereco endereco)
+        {
+            _unit.EnderecoRepository.Cadastrar(endereco);
+            _unit.Salvar();
+            TempData["msg"] = "Endereço Cadastrado com Sucesso";
+
+
+            return RedirectToAction("Cadastrar");
+        }
+    }
+}
